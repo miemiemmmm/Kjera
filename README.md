43ec82d6570f2d6a29f33617aa19bd9341d246d5
Yang Zhang <y.zhang@bioc.uzh.ch>
2024-02-26 15:47:35 +0100

    Added a major update to the example slide.

--- README.md
+++ README.md
@@ -1,78 +1,93 @@
 # Kjera
 
-----------------------------------------------------------------------------------------------
+Welcome to **[Kjera](https://github.com/miemiemmmm/Kjera)**, a versatile template designed to seamlessly embed 3D objects and molecules into your presentations. Kjera leverages the capabilities of modern web technologies to create interactive and engaging presentations. 
+To make the most out of the Kjera template, users should have a basic understanding of HTML and JavaScript which enables you to customize your presentations and integrate 3D models effectively.
 
-- <strong>This Repo is still under active construction. Functions might experience significant change </strong>
-- <strong>Please use in cautious</strong>
+<!-- This repository will be work as a template for you to make your own presentation. 
+As demonstrated in its name, its uses [Reveal.js](https://revealjs.com/) as the presentation framework and [Three.js](https://threejs.org/) as a 3D engine. 
+There are also [3Dmol.js](https://3dmol.csb.pitt.edu/) for molecular visualization.  -->
 
-----------------------------------------------------------------------------------------------
-
-Welcome to the **Kjera(Kinetic JavaScript Embeds for Reveal.js and molecular Animation)** repository!
-This is an experimental repository. The author tried many ways to embed 3D objects/molecules in his 
-presentation, however, none of them was satisfactory. The author decided to make his own framework. 
-
-This repository will be work as a template for you to make your own presentation. As demonstrated in 
-its name, its uses [Reveal.js]() as a presentation framework and [three.js]() as a 3D engine. There 
-are also [3DMol.js]() for molecular visualization. 
-
-You do not have to be an expert in JavaScript to successfully use this template.
-
-HTML/Reveal.js based template for slides presentation
-
-Animation of your molecular dynamics trajectory is on the schedule. But no one knows when the author
-will release it.
-
-## Features
-It is good to know what you are getting into, so here is a list of JavaScript libraries used in this template:
-
-Github works as the hosting service for all of the resources of the presentation.
-
+## Key Features
+- Modern Web Standards and Libraries 
+- Interactive Elements 
+- Smooth Presentation Delivery 
+- 3D Molecular Rendering 
+- 3D Object Rendering 
+- Mathematical Expressions 
+- Dynamic Content Loading via GitHub API 
 
 
 ## Usage
-The indexing of the file is done by the resource (typically files you might request remotely), so you can use the 
-same file name for different files in different folders.
 
+Follow the steps below to use this template:
+1. Click the "Use this template" button to create a new repository on GitHub
+2. Set up the new GitHub repository name, description and visibility
+3. Clone the new repository to your local machine
+4. Check the [example slides](example_slides.html) for demonstration and further customization
 
-1. Copy this repository
-2. Change the name of the folder to your presentation name and Push the repository to your own GitHub account (Or fork the repo)
-3. If the repository is private while you are working on it, generate an [access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens) for this repository
-4. Drop your images, molecule, 3D object files in folder. We will index these files by their names(url).
-5. 
+The example slides is also hosted [HERE](https://miemiemmmm.b-cdn.net/Kjera/example_slides.html) for preview.
 
-### Styling and positioning of the elements
+<!-- The indexing of the file is done by the resource (typically files you might request remotely), so you can use the same file name for different files in different folders. -->
+<!-- 3. If the repository is private while you are working on it, generate an [access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens) for this repository -->
+<!-- 4. Drop your images, molecule, 3D object files in folder. We will index these files by their names (url). -->
 
-### Adding a new slide
-Under the "slide_container" (with class as "slides"), adding "section" with "data-state" attribute. 
+### Adding New Slides
+Under the **slide_container**, each **section** stands for a slide. Set the **data-state** attribute for further callback customization.
 ```
 <!--Template page-->
-<section data-state="slide_">
-    <p class="slide-title">This is a Template Slide for Demonstration</p>
-    <div class="slide_content_style1">
-        <p>This is a Template Main Context1</p>
-        <p>This is a Template Main Context2</p>
-        <p>This is a Template Main Context3</p>
-    </div
-    <p class="reference_style">This is a template reference.</p
+<section data-state="slide_id">
+    <p class="slide-title">Template Slide for Demonstration</p>
+    <div class="box_style_rightbound">
+        <p class="slide_item_style1">Context 1</p>
+        <p class="slide_item_style1">Context 2</p>
+        <p class="slide_item_style1">Context 3</p>
+    </div>
 </section>
 ```
 
-### Adding an image or gif animation 
+### Positioning Text Boxes
+Check the [style.css](styles.css) for the pre-defined box types (class name start with "box_style_"). 
 
-### Gallery 
+To defined a new box type, add the following code to the [style sheet](styles.css). 
+```
+.box_style_custom{
+    position: absolute;
+    inset: 50% 60% 10% 10% !important; /* manually set the borders */
+    padding: 10px !important;
+    font-size: 20px;
+    text-align: center;
+}
+```
 
-### Laser pointer
+### Slide-Specific Callback Function
+```
+Reveal.addEventListener( 'slidechanged', function( event ) {
+    if (event.currentSlide && event.currentSlide.getAttribute('data-state') == "slide_id") {  // Replace here the slide data-state
+        // Your code here
+    }
+});
+```
 
-### Embedding a 3D objects
+## Kind Recommendations
+- Use a smart IDE or AI copilot to help you write the code and focus on writing the content
+- Use a local server to preview your slides
+- Different screen resolutions may affect the layout, might need to zoom in/out to adjust the layout
 
-### Embedding a 3D molecule
 
+## Readmap 
+1. Support more 3D object file types
+2. Animation of molecular dynamics trajectories
 
-## Contribution
-Contributions are always welcome! Please contact the author if you develop a awsome features or propose a better way 
-of doing things.
-
+If you have more awesome ideas, please let me know by [creating an issue](https://github.com/miemiemmmm/Kjera/issues).
 
 ## License
 This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+Without the following libraries, this project would not be possible:<br>
+<a href="https://revealjs.com/" target="_blank">Reveal.js</a>: Presentation framework<br>
+<a href="https://threejs.org/" target="_blank">Three.js</a>: 3D object rendering<br>
+<a href="https://3dmol.csb.pitt.edu/" target="_blank">3Dmol.js</a>: 3D molecule rendering<br>
+<a href="https://mathjax.org/" target="_blank">MathJax</a>: LaTeX rendering<br>
+<a href="https://jquery.com/" target="_blank">jQuery</a>: DOM element manipulation<br>
 
--- modules.js
+++ modules.js
@@ -1,173 +1,387 @@
 import * as THREE from  "https://cdn.jsdelivr.net/npm/three@0.127.0/build/three.module.js"
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.127.0/examples/jsm/controls/OrbitControls.js"
 import { PLYLoader } from "https://cdn.jsdelivr.net/npm/three@0.127.0/examples/jsm/loaders/PLYLoader.js"
-console.log(THREE)
-console.log(OrbitControls)
-console.log(PLYLoader)
 
 $.getScript("https://cdn.plot.ly/plotly-2.25.2.min.js")
 $.getScript("https://3Dmol.org/build/3Dmol-min.js")
 
+const MIMETYPES = {
+  'png': 'image/png', 'jpeg': 'image/jpeg', 'jpg': 'image/jpeg',
+  'gif': 'image/gif', 'bmp': 'image/bmp', 'tiff': 'image/tiff',
+  'tif': 'image/tiff', 'webp': 'image/webp', 'svg': 'image/svg+xml'
+};
+
+const IMAGEFORMATS = Object.keys(MIMETYPES);
+
+var BACKGROUND_COLOR = "#FFF9DE"
 
 class github_api {
-    constructor() {
-        this.repo = "";
-        this.accept = "application/vnd.github+object";
-        this.authorization = ""; // "Bearer "+token
+  constructor() {
+    this.repo = "";
+    this.accept = "application/vnd.github+object";
+    this.authorization = "";
+  }
+  have_auth(){
+    return this.authorization.length > 0
+  }
+  get_auth() {
+    var ret_headers = {
+      Accept: this.accept,
     }
-    get_auth() {
-        var ret_headers = {
-            Accept: this.accept,
-        }
-        if (this.authorization.length > 0){
-            ret_headers.Authorization = this.authorization
-        }
-        return ret_headers
+    if (this.authorization.length > 0){
+      ret_headers.Authorization = this.authorization
     }
-    set_authorization(token) {
-        this.authorization = "Bearer "+token
-    }
-    set_repo(repo_url) {
-        this.repo = repo_url
-    }
+    return ret_headers
+  }
+  set_authorization(token) {
+    this.authorization = "Bearer "+token
+  }
+  set_repo(repo_url) {
+    this.repo = repo_url
+  }
 }
 const github_auth = new github_api();
-const imageFormats = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'webp', 'svg'];
 
 function set_token(token) {
-    github_auth.set_authorization(token)
+  github_auth.set_authorization(token)
 }
 function set_repo(repo_url) {
-    github_auth.set_repo(repo_url)
+  github_auth.set_repo(repo_url)
+}
+function set_bgcolor(color){
+  BACKGROUND_COLOR = color
 }
 
-const testfunc = function(){
-    console.log("test function");
+
+async function getUrlContents(url){
+  // Fetch file from Github via content API
+  // Assume it is the file path to the image in the repo
+  var ret;
+  if (github_auth.have_auth() == false) {
+    console.log("No authorization token")
+    ret = await fetch(url);
+  } else {
+    ret = await fetch(url, {headers: github_auth.get_auth()});
+  }
+  const data = await ret.json();
+  if (data.hasOwnProperty("content") && data.content.length > 0){
+    /* Small files for which the content is returned directly */
+    console.log("Download file from github (Content): ", url);
+    return data.content
+  } else if (data.hasOwnProperty("git_url")){
+    console.log("Download file from github (Blob): ", data.git_url);
+    let ret_blob = await fetch(data.git_url, {headers: github_auth.get_auth()});
+    let data2 = await ret_blob.json();
+    return data2.content
+  } else if (data.hasOwnProperty("sha")){
+    /* This route is mainly for big files */
+    const blob_url =  url.replace(/contents.*/, "git/blobs")+"/"+data.sha;
+    console.log("Download file from github: route 3", blob_url);
+    let ret_blob = await fetch(blob_url, {headers: github_auth.get_auth()});
+    let data2 = await ret_blob.json();
+    return data2.content
+  }
 }
 
-async function getContents(url){
-    // Fetch file from Github via content API
-    const ret = await fetch(url, {headers: github_auth.get_auth()});
-    const data = await ret.json();
-    if (data.hasOwnProperty("content") && data.content.length > 0){
-        console.log("Download file from github: route 1", url);
-        return data.content
-    } else if (data.hasOwnProperty("git_url")){
-        console.log("Download file from github: route 2", data.git_url);
-        let ret_blob = await fetch(data.git_url, {headers: github_auth.get_auth()});
-        let data2 = await ret_blob.json();
-        return data2.content
-    } else if (data.hasOwnProperty("sha")){
-        const blob_url =  url.replace(/contents.*/, "git/blobs")+"/"+data.sha;
-        console.log("Download file from github: route 3", blob_url);
-        let ret_blob = await fetch(blob_url, {headers: github_auth.get_auth()});
-        let data2 = await ret_blob.json();
-        return data2.content
-    }
-}
+
 
 async function getImageb64(fileurl) {
-    // Read image Github URL to base64 encoded string
-    try {
-        const data = await getContents(fileurl);
-        let format = fileurl.split('/').pop().split('.').pop().toLowerCase(); // Convert to lowercase
-        const mimeTypes = {
-            'png': 'image/png', 'jpeg': 'image/jpeg', 'jpg': 'image/jpeg', 'gif': 'image/gif',
-            'bmp': 'image/bmp', 'tiff': 'image/tiff', 'webp': 'image/webp', 'svg': 'image/svg+xml'
-        };
-        const dataUriPrefix = mimeTypes[format];
-        if (!dataUriPrefix) {
-            throw new Error('Unsupported image format');
-        }
-        return `data:${dataUriPrefix};base64,${data}`;
-    } catch (error) {
-        console.error('An error occurred while fetching the file:', error);
-    }
+  // Read image Github URL to base64 encoded string
+  const supported_image_types = Object.keys(MIMETYPES);
+  // Convert the format to lowercase
+  const format = fileurl.split('/').pop().split('.').pop().toLowerCase();
+  if (!supported_image_types.includes(format)) {
+    throw new Error('Unsupported image format: '+format);
+  }
+  try {
+    const data = await getUrlContents(fileurl);
+    const dataUriPrefix = MIMETYPES[format];
+    return `data:${dataUriPrefix};base64,${data}`;
+  } catch (error) {
+    console.error('An error occurred while fetching the file:'+fileurl+'; Because of ', error);
+  }
 }
 
+function sleep(ms) {
+  console.log("Sleeping for ", ms, "ms")
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 async function setupImage(image, figContent){
-    // Setup an image to an Element ID or HTMLElement figContent could be based64 encoded string, Github URL,
-    // or the file path to the image in the repo
-    var img;
-    // Check if image is an HTMLElement (like what's returned by document.getElementById)
-    if (image instanceof HTMLElement) {
-        img = image;
-        console.log("Setting up an HTMLElement")
-    } else if (typeof image === 'string') {
-        // If it's not, assume it's an id
-        img = document.getElementById(image);
-        if (img == null) {
-            console.log("No such image id: ", image, " in the slide.")
-            return;
-        }
+  // Setup an image to an Element ID or HTMLElement figContent could be based64 encoded string, Github URL,
+  // or the file path to the image in the repo
+  var img;
+  // Check if image is an HTMLElement (like what's returned by document.getElementById)
+  if (image instanceof HTMLElement) {
+    img = image;
+    console.log("Setting up an HTMLElement")
+  } else if (typeof image === 'string') {
+    // If it's not, assume it's an id
+    img = document.getElementById(image);
+    if (img == null) {
+      console.log("No such image id: ", image, " in the slide.")
+      return;
     }
-    console.log("Setting up image: ", img)
+  }
+  if (figContent.startsWith("data")){
+    console.log("Setting up image via base64 encoded string")
+    img.src = figContent;
+  } else if (figContent.startsWith("http") && figContent.includes("github.com")) {
+    console.log("Setting up image via github url")
+    const fig_content = await getImageb64(figContent);
+    img.src = fig_content;
+  } else if (figContent.startsWith("http") && ! figContent.includes("github.com")) {
+    console.log("Setting up image via non-github url")
+    img.src = figContent;
+  } else if (IMAGEFORMATS.includes(figContent.split('.').pop().toLowerCase())) {
+    console.log("Setting up image via file path in the repo")
+    const fig_content = await getImageb64(github_auth.repo + figContent);
+    img.src = fig_content;
+  } else {
+    // Actual file in the root folder as input
+    img.src = figContent;
+  }
+}
 
-    if (figContent.startsWith("data")){
-        // Base64 encoded image as input
-        img.src = figContent;
-    } else if (figContent.startsWith("http")) {
-        // Assume it is the file path to the image in the repo
-        const fig_content = await getImageb64(figContent);
-        console.log("Setting up image: ", img, fig_content)
-        img.src = fig_content;
-    } else if (imageFormats.includes(figContent.split('.').pop().toLowerCase())) {
-        // Image file url as input
-        const fig_content = await getImageb64(github_auth.repo + figContent);
-        img.src = fig_content;
-    } else {
-        // Actual file in the root folder as input
-        img.src = figContent;
+async function listAndFetch(theurl){
+  var ret_data = {}
+  let response = await fetch(theurl, {headers: github_auth.get_auth()});
+  let data = await response.json();
+  if (!Array.isArray(data)) {
+    console.log("Warning: Failed to retrieve the contents as an array: ", data)
+    return ret_data
+  }
+  for (let file of data) {
+    if (file.type == "dir"){
+      var newpath = theurl+file.name+"/"
+      let tempdata = await listAndFetch(newpath)
+      for (let key in tempdata){
+        ret_data[key] = tempdata[key]
+      }
+    } else if (file.type == "file" && IMAGEFORMATS.includes(file.name.split('.').pop().toLowerCase())){
+      ret_data[file.name] = await getImageb64(theurl + file.name);
     }
+  }
+  return ret_data
 }
 
 async function getAllImages(FIGURES) {
-    // Get all images from the repo asynchonously
-    // Lazy mode: Automatically get all images from the repo; Save to the global object: FIGURES
-    try {
-        // Get the contents of the repo
-        let response = await fetch(github_auth.repo, {headers: github_auth.get_auth()});
-        let data = await response.json();
-        // For each file in the repo
-        // 1. Check image;
-        // 2. Fetch the image and store the image in base64 format
-        for (let file of data) {
-            let extension = file.name.split('.').pop().toLowerCase()
-            if (imageFormats.includes(extension)) {
-                var filename = file.name.split('?')[0].split("/")[file.name.split('?')[0].split("/").length-1];
-                FIGURES[filename] = await getImageb64(github_auth.repo + filename);
-            }
-        }
-        // console.log(FIGURES)
-    } catch (error) {
-        console.error("An error occurred:", error);
-    }
+  // Get all images from the repo asynchonously
+  // Lazy mode: Automatically get all images from the repo; Save to the global object: FIGURES
+  // try {
+  // Get the contents of the repo
+  let tempimgs = await listAndFetch(github_auth.repo);
+  for (let key in tempimgs){
+    FIGURES[key] = tempimgs[key]
+  }
+  // await sleep(2000);
+  // } catch (error) {
+  //   console.error("An error occurred:", error);
+  // }
 }
 
-function getFileName(element){
-    // ??????????????????????????????? Is this necessary? //
-    /////////////////////////////////////////////////
-    // Shortcut function to resolve the file name of an element
-    // The <src> attribute of the image element should be the name of the desired image file
-    /////////////////////////////////////////////////
-    if (typeof element === 'string' || element instanceof String){
-        let elem = document.getElementById(element);
-        // let src_str = elem.src.split("?")[0];
-        return elem.src.split("/")[elem.src.split("/").length-1]
-    } else if (element instanceof HTMLElement){
-        let elem = element;
-        return elem.src.split("/")[elem.src.split("/").length-1]
+
+
+function linspace(start, end, num) {
+  const step = (end - start) / (num - 1);
+  const result = new Array(num);
+  for (let i = 0; i < num; i++) {
+    result[i] = start + i * step;
+  }
+  return result;
+}
+
+function renderGridPoints(viewer, center, lengths, dims) {
+  const sphereRadius = 0.1;
+  const sphereColor = 0xff0000;
+  var xs = linspace(center[0]-lengths[0]/2, center[0]+lengths[0]/2, dims[0]);
+  var ys = linspace(center[1]-lengths[1]/2, center[1]+lengths[1]/2, dims[1]);
+  var zs = linspace(center[2]-lengths[2]/2, center[2]+lengths[2]/2, dims[2]);
+  var newshape = viewer.addShape({color: sphereColor})
+  for (let x = 0; x < xs.length; x++) {
+    for (let y = 0; y < ys.length; y++) {
+      for (let z = 0; z < zs.length; z++) {
+        //combinedShape.addSphere({ center: { x: xs[x], y: ys[y], z: zs[z] }, radius: sphereRadius, color: sphereColor });
+        newshape.addSphere({ center: { x: xs[x], y: ys[y], z: zs[z] }, radius: sphereRadius});
+      }
     }
+  }
+  viewer.zoomTo();
+  viewer.render();
+}
+
+function resetCanvas(parent_div, viewer){
+  var container = document.getElementById(parent_div);
+  var width = container.offsetWidth;
+  var height = container.clientHeight;
+  console.log("Set the width/height of the canvas: ",parent_div, "height/width:", height, width)
+  viewer.setWidth(width);
+  viewer.setHeight(height);
+  viewer.render();
+  // $("#"+parent_div+" canvas").css({"position": "relative", "width":"100%", "height": "100%"})
+}
+
+async function add3DMolObject(divid, fileurl){
+  var container = document.getElementById(divid);
+  var viewer = $3Dmol.createViewer(container);
+  let format = fileurl.split("/").pop().split(".").pop();
+  console.log("Adding 3D molObj: Reading the file as ", format)
+
+  var mol_string;
+  if (fileurl.includes("github.com")){
+    mol_string = atob(await getUrlContents(fileurl))
+  } else {
+    mol_string = await fetchFileFromCDN(fileurl);
+  }
+  
+  viewer.addModel(mol_string, format);
+  viewer.setStyle({}, { stick: {} });
+  viewer.setBackgroundColor(BACKGROUND_COLOR); 
+  // Zoom to fit the molecule in the viewer and Render the molecule
+  resetCanvas(divid, viewer);
+  viewer.zoomTo();
+  viewer.render();
+  return viewer;
+}
+
+async function addModel(viewer, fileurl, style={}){
+  let format = fileurl.split("/").pop().split(".").pop();
+  console.log("Adding 3D molObj: Reading the file as ", format)
+
+  var mol_string;
+  if (fileurl.includes("github.com")){
+    mol_string = atob(await getUrlContents(fileurl))
+  } else {
+    mol_string = await fetchFileFromCDN(fileurl);
+  }
+  
+  viewer.addModel(mol_string, format);
+  // viewer.setStyle({}, { stick: {} });
+  viewer.setBackgroundColor(BACKGROUND_COLOR); 
+  // Zoom to fit the molecule in the viewer and Render the molecule
+  viewer.zoomTo();
+  viewer.render();
+  return viewer;
+}
+
+async function fetchFileFromCDN(fileurl){
+  // Fetch file from Github via content API
+  // Assume it is the file path to the image in the repo
+  const ret = await fetch(fileurl);
+  if (!ret.ok){
+    console.log("Failed to fetch the file from CDN: ", fileurl)
+    return ""
+  } else {
+    return ret.text()
+  }
+}
+
+
+async function initPLYObject(divid, fileurl){
+  // Initialize the scene with a ply object
+  const THREE = window.THREE;
+  const OrbitControls = window.OrbitControls ;
+  // Setup for Renderer
+  const renderer = new THREE.WebGLRenderer();
+  var camera
+  if (divid.length > 0){
+    var container = document.getElementById(divid);
+    console.log("using divid", divid, "Width", container.clientWidth, "Height: ", container.clientHeight)
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    container.appendChild(renderer.domElement);
+    const aspectRatio = container.clientWidth / container.clientHeight;
+    camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+    camera.position.z = 5;
+  } else {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+  }
+  // Setup for the Scene, Background color, and light
+  const scene = new THREE.Scene();
+  renderer.setClearColor(BACKGROUND_COLOR);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // 0.5 intensity
+  scene.add(ambientLight);
+  const light = new THREE.DirectionalLight(0xffffff, 1, 0);
+  light.position.set(10, 10, 10);
+  scene.add(light);
+
+  // Add Orbit controler
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // Convert the content from base 64 to string
+  console.log("Loading the plyfile", fileurl)
+  const plycontent = await fetchFileFromCDN(fileurl);
+  var mesh = loadPLYMesh(plycontent, scene, camera, light);
+
+  scene.add(mesh);
+  camera.position.set(0, 0, 20);
+  camera.near = 0.1;
+  camera.far = 1000;
+  camera.updateProjectionMatrix();
+  return [scene, renderer, camera, light]
+}
+
+
+function loadPLYMesh(plycontent, scene, camera, light) {
+  // Load PLY file from base64 encoded string and return mesh
+  const loader = new PLYLoader();
+  const geometry = loader.parse(plycontent);
+  geometry.computeVertexNormals();
+  const material = new THREE.MeshStandardMaterial({vertexColors: true, flatShading: true});
+  const mesh = new THREE.Mesh(geometry, material);
+  geometry.computeBoundingBox();
+  const boundingBox = geometry.boundingBox;
+  const center = boundingBox.getCenter(new THREE.Vector3());
+  mesh.position.set(-center.x, -center.y, -center.z);
+  mesh.onBeforeRender = function (renderer, scene, camera) {
+    light.position.copy(camera.position);
+  };
+  return mesh
+}
+
+async function addPLYtoStage(fileurl, scene, renderer, camera, light, offsets){
+  const plycontent = atob(await getUrlContents(fileurl));
+  var themesh = loadPLYMesh(plycontent, scene, camera, light);
+  themesh.geometry.computeBoundingBox();
+  const boundingBox = themesh.geometry.boundingBox;
+  const center = boundingBox.getCenter(new THREE.Vector3());
+  themesh.position.set(-center.x + offsets[0], -center.y + offsets[1], -center.z +offsets[2]);
+  scene.add(themesh);
+  return [scene, renderer, camera, light]
+}
+
+
+function testfunc(){
+  // Test function for the module development
+  console.log("test function");
 }
 
 
 export {
-    testfunc,
-    getContents,
-    getImageb64,
-    set_token,
-    setupImage,
-    getAllImages,
-    set_repo,
+  testfunc,
+  sleep,
+
+  // Setup global variables 
+  set_repo,
+  set_token,
+  set_bgcolor,
+
+  // Fetch the contents 
+  getUrlContents,
+  getImageb64,
+  
+  setupImage,
+  getAllImages,
+
+
+  // Render the molecule and set the canvas for 3D objects
+  initPLYObject,
+  addPLYtoStage,
+  add3DMolObject,
+  addModel,
+  renderGridPoints,
+  resetCanvas,
+  
 }
